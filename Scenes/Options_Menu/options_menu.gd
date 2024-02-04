50adf54b4a3c89e967947fdc822df0f32d1d7b73
mayleenl <mayleen.liu@gmail.com>
2024-02-03 23:25:04 -0500

    basic main menu

---
+++ Scenes/Options_Menu/options_menu.gd
@@ -0,0 +1,15 @@
+class_name OptionsMenu
+extends Control
+
+@onready var exit_button = $MarginContainer/VBoxContainer/Exit_Button as Button
+
+
+signal exit_options_menu
+
+func _ready():
+	exit_button.button_down.connect(on_exit_pressed)
+	set_process(false)
+
+func on_exit_pressed():
+	exit_options_menu.emit()
+	set_process(false)
